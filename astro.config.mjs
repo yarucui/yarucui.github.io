58a7518f0e8a3154a3b12bfda36bc200f9630d16
Yaru Cui <sisucyrcyr@gmail.com>
2026-02-14 23:35:29 +0100

    fixed base(repo name) in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
   site: 'https://yarucui.github.io', 
-  base: '/my-aesthetic-site/', // Change this to '/repo-name/' ONLY if your repo is NOT 'username.github.io'
+  base: '/yarucui.github.io/', // Change this to '/repo-name/' ONLY if your repo is NOT 'username.github.io'
   // markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
