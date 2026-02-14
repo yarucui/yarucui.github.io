bd569767473f17fa77d52eda8b77a67261c39bbf
Yaru Cui <sisucyrcyr@gmail.com>
2026-02-14 23:44:44 +0100

    fixed syntax error in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://yarucui.github.io', 
-  base: '/yarucui.github.io/', // Change this to '/repo-name/' ONLY if your repo is NOT 'username.github.io'
-  // markdown: {
+  site: 'https://yarucui.github.io',
+  base: '/yarucui.github.io/',
+  markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
   },
